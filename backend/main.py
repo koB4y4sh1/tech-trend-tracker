aa61613064dfe2f7fe7f11c977127689fa977110
koB4y4sh1 <k_kobakenjp@yahoo.co.jp>
2025-05-05 00:56:41 +0000

    :construction_worker: 開発効率の向上 起動コマンドを秘匿化し、「python3 main.py」でサーバーが起動できるようしました。

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+def main():
+    uvicorn.run("application:app", host="0.0.0.0", port=8000, reload=True)
+
+if __name__ == "__main__":
+    main()
