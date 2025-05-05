53105e9aea6ebe0f9f7e977e7f65806d8483145f
koB4y4sh1 <k_kobakenjp@yahoo.co.jp>
2025-05-05 20:00:28 +0900

    :wrench: CORS設定の追加 バックエンドのCORS設定を追加し、FEとBEの通信を確立しました。

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,33 @@
-from fastapi import FastAPI
+import os
+
 import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv 
+
+# .env を読み込む（ローカル開発時のみ有効）
+load_dotenv()
 
 app = FastAPI()
+
+# CORS設定
+origins = [
+    "http://localhost:3000",
+    os.environ.get("WEB_BASE_URL"),
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
 
-def main():
-    uvicorn.run("application:app", host="0.0.0.0", port=8000, reload=True)
-
+# ローカル開発時のみ`py main.py`で起動する
 if __name__ == "__main__":
-    main()
+    uvicorn.run("application:app", host="0.0.0.0", port=8000, reload=True)
