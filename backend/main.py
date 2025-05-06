85fe7b1eb29590f49be06d4987599d1a1af2250a
koB4y4sh1 <k_kobakenjp@yahoo.co.jp>
2025-05-06 09:28:54 +0900

    :bug: BE起動失敗時のエラー解消

--- backend/main.py
+++ backend/main.py
@@ -30,4 +30,4 @@ def read_root():
 
 # ローカル開発時のみ`py main.py`で起動する
 if __name__ == "__main__":
-    uvicorn.run("application:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
