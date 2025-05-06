ec6cdc613c79c0655e7a915d572a8f8b9582d2ea
koB4y4sh1 <k_kobakenjp@yahoo.co.jp>
2025-05-06 09:24:40 +0900

    :wrench: FEの構成変更・Eslint/Prettier VSCodeの設定を更新し、自動フォーマットを無効化しました。また、ESLintの設定を強化し、依存関係を更新しました。

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -22,13 +22,13 @@ module.exports = {
   ignorePatterns: ["!**/.server", "!**/.client"],
 
   // Base config
-  extends: ["eslint:recommended"],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
 
   overrides: [
     // React
     {
-      files: ["**/*.{js,jsx,ts,tsx}"],
-      plugins: ["react", "jsx-a11y"],
+      files: ['**/*.{js,jsx,ts,tsx}'],
+      plugins: ['react', 'jsx-a11y'],
       extends: [
         "plugin:react/recommended",
         "plugin:react/jsx-runtime",
@@ -45,7 +45,7 @@ module.exports = {
           { name: "NavLink", linkAttribute: "to" },
         ],
         "import/resolver": {
-          typescript: {},
+          typescript: "./tsconfig.json",
         },
       },
     },
@@ -59,11 +59,11 @@ module.exports = {
         "import/internal-regex": "^~/",
         "import/resolver": {
           node: {
-            extensions: [".ts", ".tsx"],
+            paths: ["app"], 
+            extensions: ['.js', '.ts', '.d.ts', '.tsx']
           },
-          typescript: {
-            alwaysTryTypes: true,
-          },
+          typescript: "./tsconfig.json"
+          ,
         },
       },
       extends: [
